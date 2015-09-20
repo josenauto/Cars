7439caa1af481608855ebb7057e1143446b18a69
josenauto <jemilionautoch@gmail.com>
2015-09-20 17:45:32 -0300

    Add some validation and modify new and create actions

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -9,14 +9,17 @@ class CarsController < ApplicationController
   end
 
   def new
-
+    @car = Car.new
   end
 
   def create
     @car = Car.new(car_params)
 
-    @car.save
-    redirect_to @car
+    if @car.save
+      redirect_to @car
+    else
+      render 'new'
+    end
   end
 
   def car_params
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,4 @@
 class Car < ActiveRecord::Base
+  validates :name, presence: true,
+                   length: { minimum: 5 }
 end
