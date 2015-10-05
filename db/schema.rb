ffb7075f0facb042443f84f862c9a045593bcadd
josenauto <jemilionautoch@gmail.com>
2015-10-05 16:56:23 -0300

    Add the small tag

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925002112) do
+ActiveRecord::Schema.define(version: 20150925001253) do
 
   create_table "cars", force: :cascade do |t|
     t.string   "name"
